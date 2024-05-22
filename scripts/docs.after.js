198f6562100130544d4391eb87234b8b9e6a59e9
Zhang YingJie <jie736805161@qq.com>
2024-05-22 14:54:42 +0800

    feat: 增加脚本

--- scripts/docs.after.js
+++ scripts/docs.after.js
@@ -1,6 +1,22 @@
-import fs from 'fs';
-import {copyFile} from '../src/Node/files'
+const fs = require('fs')
 
+/**
+ * 复制文件
+ * @param {*} source 源文件
+ * @param {*} target 目标文件
+ */
+const copyFile = (source, target) => {
+  if (fs.existsSync(target)) {
+    console.log('目标文件已存在!');
+    return;
+  }
+  try {
+    fs.writeFileSync(target, fs.readFileSync(source));
+    console.log('copy file success!');
+  } catch (err) {
+    console.log('copy file fail!', err);
+  }
+};
 // 将源文件复制到html文件夹下  保证在页面端控制台直接使用库里面的方法
 copyFile('../src/iife/index.min.js', '../html/js-tools-xxx.js')
 copyFile('./docs.onload.js', '../html/docs.onload.js')
