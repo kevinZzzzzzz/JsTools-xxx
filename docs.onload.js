3e99728dd632e2d1cdda528fcd934efa608079c3
kevinZzzzzzz <kevinZzzzzzz@users.noreply.github.com>
2024-05-22 08:00:25 +0000

    feat: save

--- docs.onload.js
+++ docs.onload.js
@@ -1,1 +1,3 @@
-console.log('welcome~')
+console.log('%c welcome~',
+'line-height:28px;padding:4px;background:#a1afc9;color:#000;font-size:16px;margin-right:15px',
+'color:#ff9900;line-height:28px;font-size:16px;',)
